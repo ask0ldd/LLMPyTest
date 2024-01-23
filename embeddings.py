cb9a41375b840f990f730a59e5145e0dc550371d
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-01-23 13:28:45 +0100

    embeddings distance calc through np

--- embeddings.py
+++ embeddings.py
@@ -5,8 +5,9 @@ from langchain.prompts import PromptTemplate
 from langchain_community.llms import LlamaCpp
 from langchain.embeddings.llamacpp import LlamaCppEmbeddings
 from langchain.evaluation import load_evaluator, EmbeddingDistance
+import numpy as np
 
-embeddings = LlamaCppEmbeddings(model_path="G:\AI\mistral-7b-instruct-v0.1.Q5_K_M.gguf", n_threads=3)
+embeddings = LlamaCppEmbeddings(model_path="G:\AI\mistral-7b-instruct-v0.1.Q5_K_M.gguf", n_threads=3, verbose=False)
 
 evaluator = load_evaluator("pairwise_embedding_distance", distance_metric=EmbeddingDistance.COSINE, embeddings=embeddings)
 
@@ -23,6 +24,10 @@ def getEmbeddingsQuery(text) :
 def getEmbeddingsDocuments(texts) :
     return embeddings.embed_documents(texts)
 
-print(getEmbeddingsDistance("cat", "car"))
 print(getEmbeddingsQuery(text))
 print(getEmbeddingsDocuments(documents))
+print(getEmbeddingsDistance("cat", "car"))
+print(getEmbeddingsDistance("cat", "because"))
+print(np.linalg.norm(np.array(getEmbeddingsDocuments("cat")[0]) - np.array(getEmbeddingsDocuments("car")[0])))
+print(np.linalg.norm(np.array(getEmbeddingsDocuments("cat")[0]) - np.array(getEmbeddingsDocuments("because")[0])))
+print(np.linalg.norm(np.array(getEmbeddingsDocuments("man")[0]) - np.array(getEmbeddingsDocuments("king")[0])))
