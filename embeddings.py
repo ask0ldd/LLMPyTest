e9d822c9645070f96a633bac1e8a5dd34911a30d
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-01-23 12:45:16 +0100

    embeddings distance

--- embeddings.py
+++ embeddings.py
@@ -4,10 +4,13 @@ from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
 from langchain_community.llms import LlamaCpp
 from langchain.embeddings.llamacpp import LlamaCppEmbeddings
+from langchain.evaluation import load_evaluator, EmbeddingDistance
 
 embeddings = LlamaCppEmbeddings(model_path="G:\AI\mistral-7b-instruct-v0.1.Q5_K_M.gguf", n_threads=3)
 
-doc_result = embeddings.embed_documents(["cat", "dog", "car"])
+evaluator = load_evaluator("pairwise_embedding_distance", distance_metric=EmbeddingDistance.COSINE, embeddings=embeddings)
+
+# doc_result = embeddings.embed_documents(["cat", "dog", "car"])
 
 '''text = "This is a test document."
 
@@ -16,4 +19,10 @@ query_result = embeddings.embed_query(text)
 doc_result = embeddings.embed_documents([text])
 
 print(query_result)'''
-print(doc_result)
+# print(doc_result)
+
+result = evaluator.evaluate_string_pairs(prediction="cat", prediction_b="car")
+print(result)
+
+result = evaluator.evaluate_string_pairs(prediction="man", prediction_b="king")
+print(result)
